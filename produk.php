89b237f3f98a070225b9ba3b2a5c1221dc82a03c
MuhammadNurHidayat07092000 <yyt08536@gmail.com>
2020-09-12 17:14:03 +0800

    class dan objek

---
+++ produk.php
@@ -0,0 +1,16 @@
+<?php 
+
+// membuat class produk penjualan
+// membuat class komik
+// membuat class game
+
+class Produk {
+
+
+}
+
+$produk1 = new Produk();
+$produk2 = new Produk();
+$produk3 = new Produk();
+
+
