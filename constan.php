2cb02df0961090405e3f3e7d9f596dd91a1eda5d
MuhammadNurHidayat07092000 <yyt08536@gmail.com>
2020-09-17 12:38:26 +0800

    constan

---
+++ constan.php
@@ -0,0 +1,19 @@
+<?php 
+
+// define('NAMA', 'Yayat');
+// echo NAMA;
+
+// echo "<br>";
+
+//BISA DISIMPAN DIDALAM KELAS
+// const UMUR = 32;
+// echo UMUR;
+
+// class Coba {
+//     const NAMA = 'Yayat';
+// }
+
+// echo Coba::NAMA;
+
+//MAGIC CONTANT
+echo __LINE__;
